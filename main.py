6221c17c14d4c782020f37ee40f381bb8109bf2e
Joshua Morton <morton.l.joshua@gmail.com>
2026-03-28 22:04:42 -0400

    Removed a dot that allows images to be saved

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-from app import App
+# from app import App
 
-app = App()
-app.mainloop()
+# app = App()
+# app.mainloop()
+
+from curl_cffi import requests
+from scraper import load_movie_image
+
+session = requests.Session(impersonate="chrome")
+
+load_movie_image("whiplash-2014", session)
